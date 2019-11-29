40947094d3cc28756a5a23b2043c2e8bfdc5a49b
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-29 17:25:53 +0530

    [component] Added 'PollCompleted'

---
+++ HeyCarChallenge/src/components/PollCompleted.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Text } from 'react-native';
+
+const PollCompleted = () => {
+    return (
+        <Text>Congratulations!</Text>
+    );
+};
+
+export default PollCompleted;
