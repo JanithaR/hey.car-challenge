cdec6eeded4e5eece093b163d98f164c691110c7
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-29 22:35:30 +0530

    [component] Updated 'PollsRadioButton' with 'status' prop

--- HeyCarChallenge/src/atoms/PollsRadioButton.js
+++ HeyCarChallenge/src/atoms/PollsRadioButton.js
@@ -2,18 +2,29 @@
 import React from 'react';
 import { RadioButton } from 'react-native-paper';
 import PropTypes from 'prop-types';
+import Colors from '../resources/colors';
 
-const PollsRadioButton = ({ value, disabled }) => {
-    return <RadioButton value={value} disabled={disabled} />;
+const PollsRadioButton = ({ value, disabled, status }) => {
+    return (
+        <RadioButton
+            value={value}
+            status={status}
+            disabled={disabled}
+            color={Colors.primaryColor}
+            uncheckedColor={Colors.primaryColorLight}
+        />
+    );
 };
 
 export default PollsRadioButton;
 
 PollsRadioButton.propTypes = {
     value: PropTypes.string.isRequired,
+    status: PropTypes.bool,
     disabled: PropTypes.bool
 };
 
 PollsRadioButton.defaultProps = {
+    status: false,
     disabled: false
 };
