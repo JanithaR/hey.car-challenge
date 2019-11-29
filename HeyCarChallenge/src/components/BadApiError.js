0883f72deadd9d28140b227e136ca5e16e8a53f4
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-29 17:25:07 +0530

    [component] Added 'BadApiError'

---
+++ HeyCarChallenge/src/components/BadApiError.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Text } from 'react-native';
+
+const BadApiError = () => {
+    return (
+        <Text>Sorry we could not load the poll</Text>
+    );
+};
+
+export default BadApiError;
