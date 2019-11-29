9763a4c828abe59f7af2360cb06f35918a553bec
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-29 22:37:04 +0530

    [colors] Added new colors

--- HeyCarChallenge/src/resources/colors.js
+++ HeyCarChallenge/src/resources/colors.js
@@ -3,6 +3,10 @@ const Colors = Object.freeze({
     largeFontColor: '#000',
     normalFontColor: '#222',
     smallFontColor: '#555',
+    primaryColor: '#467bf6',
+    primaryColorLight: '#e0e8fd',
+    primaryColorLighter: '#f0f3fe',
+    secondaryColor: '#eb994b'
 });
 
 export default Colors;
