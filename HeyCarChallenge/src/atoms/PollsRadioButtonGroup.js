b53f7e14a6a5b03210e3d3c912ac737c7d79e3be
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-29 15:57:35 +0530

    [component] Added 'PollsRadioButtonGroup'

---
+++ HeyCarChallenge/src/atoms/PollsRadioButtonGroup.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import { RadioButton } from 'react-native-paper';
+import PropTypes from 'prop-types';
+
+const PollsRadioButtonGroup = ({ choices, onValueChange, value }) => {
+    return (
+        <RadioButton.Group onValueChange={onValueChange} value={value}>
+
+            {choices}
+
+        </RadioButton.Group>
+    );
+};
+
+PollsRadioButtonGroup.propTypes = {
+    choices: PropTypes.arrayOf(PropTypes.element).isRequired,
+    onValueChange: PropTypes.func.isRequired,
+    value: PropTypes.string,
+};
+
+PollsRadioButtonGroup.defaultValues = {
+    value: '',
+};
+
+export default PollsRadioButtonGroup;
