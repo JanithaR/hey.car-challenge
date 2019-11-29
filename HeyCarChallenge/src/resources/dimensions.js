1525840e0cc33b65c63f57b01e0ca75bb53ffd3f
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-29 15:50:44 +0530

    [resource] Added 'Dimensions'

---
+++ HeyCarChallenge/src/resources/dimensions.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+const Dimensions = Object.freeze({
+    largeFontSize: 20,
+    normalFontSize: 14,
+    smallFontsize: 10,
+});
+
+export default Dimensions;
