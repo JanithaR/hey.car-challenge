25c1d57ca2370ce1deb52ef4d40a4d01d707732d
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-29 23:34:32 +0530

    [component] Updated failing 'Choice' stories

--- HeyCarChallenge/src/components/Choice.stories.js
+++ HeyCarChallenge/src/components/Choice.stories.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import Choice from './Choice';
-import pollsRadioButton from '../atoms/PollsRadioButton.stories';
+import PollsRadioButton from '../atoms/PollsRadioButton';
 
 storiesOf('Choice', module)
     .addDecorator(withKnobs)
     .add('default', () => (
         <Choice
             label={text('Label', 'This is a choice')}
-            pollsRadioButton={pollsRadioButton}
+            pollsRadioButton={<PollsRadioButton value={'Value'} />}
             highlighted={boolean('Hightlighted', false)}
         />
     ));
