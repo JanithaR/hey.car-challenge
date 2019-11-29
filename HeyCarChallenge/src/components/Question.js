7ea4b99b3c7be03288a371ebff3987153780c8c4
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-29 22:49:46 +0530

    [component] Wrapped 'Question' Text with a container

--- HeyCarChallenge/src/components/Question.js
+++ HeyCarChallenge/src/components/Question.js
@@ -1,19 +1,34 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import PropTypes from 'prop-types';
 import Colors from '../resources/colors';
 import Dimensions from '../resources/dimensions';
 
 const Question = ({ text }) => {
-    return <Text style={styles.text}>{text}</Text>;
+    return (
+        <View style={styles.containerStyle}>
+
+            <Text style={styles.text}>
+
+                {text}
+
+            </Text>
+
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
     text: {
         color: Colors.largeFontColor,
         fontSize: Dimensions.largeFontSize,
+        fontWeight: 'bold'
     },
+    containerStyle: {
+        paddingTop: 5,
+        paddingBottom: 5
+    }
 });
 
 Question.propTypes = {
