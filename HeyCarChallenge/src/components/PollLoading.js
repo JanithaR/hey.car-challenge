f1155c8dcbce5d25ad2043173417474172d33a21
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-29 17:26:08 +0530

    [component] Added 'PollLoading'

---
+++ HeyCarChallenge/src/components/PollLoading.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Text } from 'react-native';
+
+const PollLoading = () => {
+    return (
+        <Text>Loading poll...</Text>
+    );
+};
+
+export default PollLoading;
