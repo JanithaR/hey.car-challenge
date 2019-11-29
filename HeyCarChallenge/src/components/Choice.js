46c299433b12fa807bde24485f423eddd3f2248f
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-29 15:53:50 +0530

    [component] Added 'Choice'

---
+++ HeyCarChallenge/src/components/Choice.js
@@ -0,0 +1,36 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+import Colors from '../res/colors';
+import Dimensions from '../res/dimensions';
+
+const Choice = ({ pollsRadioButton, label }) => {
+    return (
+        <View style={styles.container}>
+
+            {pollsRadioButton}
+
+            <Text style={styles.text}>{label}</Text>
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    text: {
+        color: Colors.normalFontColor,
+        fontSize: Dimensions.normalFontSize,
+    },
+});
+
+Choice.propTypes = {
+    pollsRadioButton: PropTypes.element.isRequired,
+    label: PropTypes.string.isRequired,
+};
+
+export default Choice;
