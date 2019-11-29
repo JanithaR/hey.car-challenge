0b6f7fd713718576c04a171836ad51ef593a663b
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-29 15:49:55 +0530

    [resource] Added 'Colors'

---
+++ HeyCarChallenge/src/resources/colors.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+const Colors = Object.freeze({
+    largeFontColor: '#000',
+    normalFontColor: '#222',
+    smallFontColor: '#555',
+});
+
+export default Colors;
