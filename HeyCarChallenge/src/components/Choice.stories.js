ba8740aa1111b8dbef7069fe44ca3668e4a34b19
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-30 15:02:53 +0530

    [storybook] Added 'Choice' stories

--- HeyCarChallenge/src/components/Choice.stories.js
+++ HeyCarChallenge/src/components/Choice.stories.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
-import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import Choice from './Choice';
 import PollsRadioButton from '../atoms/PollsRadioButton';
 import { RadioButtonStatus } from '../Enums';
@@ -13,6 +13,8 @@ storiesOf('Choice', module)
             label={text('Label', 'This is a choice')}
             pollsRadioButton={<PollsRadioButton value={'Value'} />}
             highlighted={boolean('Hightlighted', false)}
+            voteCount={number('Vote count', 99)}
+            voteCountVisible={boolean('Vote count visible', false)}
         />
     ))
     .add('not highlighted', () => (
@@ -27,5 +29,21 @@ storiesOf('Choice', module)
             label={'This is a choice'}
             pollsRadioButton={<PollsRadioButton value={'Value'} status={RadioButtonStatus.CHECKED} />}
             highlighted={true}
+        />
+    ))
+    .add('not highlighted vote count visible', () => (
+        <Choice
+            label={'This is a choice'}
+            pollsRadioButton={<PollsRadioButton value={'Value'} />}
+            highlighted={false}
+            voteCountVisible={true}
+        />
+    ))
+    .add('highlighted vote count visible', () => (
+        <Choice
+            label={'This is a choice'}
+            pollsRadioButton={<PollsRadioButton value={'Value'} status={RadioButtonStatus.CHECKED} />}
+            highlighted={true}
+            voteCountVisible={true}
         />
     ));
