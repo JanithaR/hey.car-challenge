65f15b996eda01f5f5cff9e0398aed91b031693c
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-30 22:10:05 +0530

    [component] Improved 'BadApiError'

--- HeyCarChallenge/src/components/BadApiError.js
+++ HeyCarChallenge/src/components/BadApiError.js
@@ -1,19 +1,37 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import PropTypes from 'prop-types';
+import Colors from '../resources/colors';
+import Dimensions from '../resources/dimensions';
 
 const BadApiError = ({ retryButton }) => {
     return (
-        <View>
+        <View style={styles.container}>
 
-            <Text>Sorry, we could not load the poll</Text>
+            <Text style={styles.text}>Sorry, we could not load the poll</Text>
 
             {retryButton}
 
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        position: 'absolute',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: Dimensions.largeFontSize,
+        color: Colors.largeFontColor
+    }
+});
 
 BadApiError.propTypes = {
     retryButton: PropTypes.element.isRequired
