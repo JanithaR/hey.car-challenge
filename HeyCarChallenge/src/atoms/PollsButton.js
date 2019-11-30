00c79b7689c784650b65d62b5f052114d3791495
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-30 22:17:09 +0530

    [component] Added prop 'testID' to 'PollsButton'

--- HeyCarChallenge/src/atoms/PollsButton.js
+++ HeyCarChallenge/src/atoms/PollsButton.js
@@ -3,13 +3,21 @@ import React from 'react';
 import { Button } from 'react-native';
 import PropTypes from 'prop-types';
 
-const PollsButton = ({ title, onPress, disabled }) => {
-    return <Button title={title} onPress={onPress} disabled={disabled} />;
+const PollsButton = ({ title, onPress, disabled, testID }) => {
+    return (
+        <Button
+            title={title}
+            testID={testID}
+            onPress={onPress}
+            disabled={disabled}
+        />
+    );
 };
 
 PollsButton.propTypes = {
     title: PropTypes.string.isRequired,
     onPress: PropTypes.func.isRequired,
+    testID: PropTypes.func.isRequired,
     disabled: PropTypes.bool
 };
 
