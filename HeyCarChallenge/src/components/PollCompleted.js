5a1ad946d09b6ec1c5b8a36aa3442565eb3361f3
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-30 10:47:52 +0530

    [component] Wrapped 'PollCompleted' Text with a container

--- HeyCarChallenge/src/components/PollCompleted.js
+++ HeyCarChallenge/src/components/PollCompleted.js
@@ -1,11 +1,33 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
+import Colors from '../resources/colors';
+import Dimensions from '../resources/dimensions';
 
 const PollCompleted = () => {
     return (
-        <Text>Congratulations!</Text>
+        <View style={styles.container}>
+
+            <Text style={styles.text}>Congratulations!</Text>
+
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        position: 'absolute',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: Dimensions.largeFontSize,
+        color: Colors.largeFontColor
+    }
+});
 
 export default PollCompleted;
