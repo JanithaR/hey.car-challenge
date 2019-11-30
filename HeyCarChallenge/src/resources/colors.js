4f00a8eff8a21973d7b9126289c23a4bdb749a23
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2019-11-30 09:55:59 +0530

    [colors] Changed 'normalFontColor'

--- HeyCarChallenge/src/resources/colors.js
+++ HeyCarChallenge/src/resources/colors.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 const Colors = Object.freeze({
     largeFontColor: '#2c3a5d',
-    normalFontColor: '#222',
+    normalFontColor: '#7d859a',
     smallFontColor: '#555',
     primaryColor: '#467bf6',
     primaryColorLight: '#e0e8fd',
